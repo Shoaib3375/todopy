3f8ad0dd1d2a7188dcb5bf870b4d524a27292b81
Md Shoaib Islam <mestabd60@gmail.com>
2025-05-13 08:08:42 +0600

    refactor data

--- models.py
+++
@@ -1,6 +0,0 @@
-from database import db
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
